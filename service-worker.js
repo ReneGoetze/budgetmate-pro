6c293afb20f943e8bbf8de6cadcdd1f3cab3cbbb
ReneGoetze <rene.goetze@icloud.com>
2025-12-04 13:22:10 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,1 @@
-const CACHE='bm-v13';
-const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.webmanifest'];
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
-});
-self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
+// placeholder sw
